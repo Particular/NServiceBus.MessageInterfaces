a0d45cf642deff2f02235d9a1b244639db351db7
kentdr <dan.kent@particular.net>
2023-10-02 15:06:15 -0400

    Fix formatting

--- src/NServiceBus.MessageInterfaces/IEvent.cs
+++ src/NServiceBus.MessageInterfaces/IEvent.cs
@@ -1,9 +1,9 @@
 ﻿namespace NServiceBus
 {
-  /// <summary>
-  /// Marker interface to indicate that a class is a event message.
-  /// </summary>
-  public interface IEvent : IMessage
-  {
-  }
+    /// <summary>
+    /// Marker interface to indicate that a class is a event message.
+    /// </summary>
+    public interface IEvent : IMessage
+    {
+    }
 }
